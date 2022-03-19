39fca2457e41225d6b7c492973051c6b9695033b
Sabrina Camargo Blockwel <63020705+sblockwel@users.noreply.github.com>
2022-03-18 21:32:08 -0300

    Tela inicial

--- app/src/main/java/com/example/jogoadivinhanumero/MainActivity.kt
+++ app/src/main/java/com/example/jogoadivinhanumero/MainActivity.kt
@@ -1,25 +1,32 @@
 package com.example.jogoadivinhanumero
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jogoadivinhanumero.ui.theme.JogoAdivinhaNumeroTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JogoAdivinhaNumeroTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    App()
                 }
             }
         }
@@ -27,14 +34,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun App() {
+    Menu()
+}
+
+@Composable
+fun ErrorScreen() {
+
+}
+
+@Composable
+fun Menu() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .fillMaxSize()
+
+    ) {
+        var numberState by remember {
+            mutableStateOf("")
+        }
+        val context = LocalContext.current
+        
+        Text(text = "Adivinhe o número ", style = MaterialTheme.typography.h4)
+        Text(text = "entre 1 e 100", style = MaterialTheme.typography.h4)
+
+        OutlinedTextField(
+            value = numberState,
+            onValueChange = {numberState = it},
+            label = { Text("Digite um número") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                if (numberState.isNotBlank()){
+                    Toast.makeText(context, "Enviado", Toast.LENGTH_SHORT).show()
+                }
+            }
+        ) {
+            Text(text = "Jogar", style = MaterialTheme.typography.h6)
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JogoAdivinhaNumeroTheme {
-        Greeting("Android")
+        App()
     }
 }
