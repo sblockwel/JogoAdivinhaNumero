8e6b4bf18df7fabd56e6a7460c9f42eb5a61337a
Sabrina Camargo Blockwel <63020705+sblockwel@users.noreply.github.com>
2022-03-25 19:58:53 -0300

    Update RandomScreen.kt

--- app/src/main/java/com/example/jogoadivinhanumero/RandomScreen.kt
+++ app/src/main/java/com/example/jogoadivinhanumero/RandomScreen.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -45,6 +47,7 @@ class RandomScreen {
             modifier = Modifier
                 .padding(all = 8.dp)
                 .fillMaxSize()
+                .background(Color(0, 0, 0))
 
         ) {
             var numberState by remember {
@@ -57,7 +60,8 @@ class RandomScreen {
             OutlinedTextField(
                 value = numberState,
                 onValueChange = { numberState = it },
-                label = { Text("Digite um número") }
+                label = { Text("Digite um número") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
             Spacer(modifier = Modifier.height(16.dp))
             Button(
